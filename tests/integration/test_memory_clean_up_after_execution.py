3af6f4d3d21501e20e96541797f145a1f14521a1
crosschainer <eodkwokm@gmail.com>
2024-04-09 16:07:29 +0000

    mem leak test

---
+++ tests/integration/test_memory_clean_up_after_execution.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+from contracting.db.driver import ContractDriver
+from contracting.execution.executor import Executor
+from contracting.config import STAMPS_PER_TAU
+from contracting.execution import runtime
+import contracting
+import gc
+import os
+import psutil
+
+def submission_kwargs_for_file(f):
+    # Get the file name only by splitting off directories
+    split = f.split('/')
+    split = split[-1]
+
+    # Now split off the .s
+    split = split.split('.')
+    contract_name = split[0]
+
+    with open(f) as file:
+        contract_code = file.read()
+
+    return {
+        'name': contract_name,
+        'code': contract_code,
+    }
+
+
+TEST_SUBMISSION_KWARGS = {
+    'sender': 'stu',
+    'contract_name': 'submission',
+    'function_name': 'submit_contract'
+}
+
+
+class TestMetering(TestCase):
+    def setUp(self):
+        # Hard load the submission contract
+        self.d = ContractDriver()
+        self.d.flush()
+
+        with open(contracting.__path__[0] + '/contracts/submission.s.py') as f:
+            contract = f.read()
+
+        self.d.set_contract(name='submission',
+                            code=contract)
+        self.d.commit()
+
+        # Execute the currency contract with metering disabled
+        self.e = Executor(driver=self.d)
+        self.e.execute(**TEST_SUBMISSION_KWARGS,
+                       kwargs=submission_kwargs_for_file('./test_contracts/currency.s.py'), metering=False, auto_commit=True)
+
+    def tearDown(self):
+        self.d.flush()
+
+    def test_memory_clean_up_after_execution(self):
+        process = psutil.Process(os.getpid())
+        before = process.memory_info().rss / 1024 / 1024
+        for i in range(500):
+            output = self.e.execute('stu', 'currency', 'transfer', kwargs={'amount': 100, 'to': 'colin'}, auto_commit=True,metering=True)
+        gc.collect()
+        after = process.memory_info().rss / 1024 / 1024
+        before_2 = process.memory_info().rss / 1024 / 1024
+        for i in range(500):
+            output = self.e.execute('stu', 'currency', 'transfer', kwargs={'amount': 100, 'to': 'colin'}, auto_commit=True,metering=False)
+        gc.collect()
+        after_2 = process.memory_info().rss / 1024 / 1024
+        print(f'RAM Difference with metering: {after - before} MB')
+        print(f'RAM Difference without metering: {after_2 - before_2} MB')
+
+        
+
+if __name__ == '__main__':
+    t = TestMetering()
+    t.setUp()
+    t.test_memory_clean_up_after_execution()
+    t.tearDown()
+   
