2580b958fba9df000e109b38ecbd0ca6d084b406
endogen <081081@gmx.net>
2024-05-31 00:53:12 +0200

    Added function to verify if a key is valid

--- src/contracting/stdlib/bridge/crypto.py
+++ src/contracting/stdlib/bridge/crypto.py
@@ -1,9 +1,11 @@
-from xian_py.wallet import verify_msg
+from xian_py.wallet import verify_msg, key_is_valid
 from types import ModuleType
 
 
 crypto_module = ModuleType('crypto')
 crypto_module.verify = verify_msg
+crypto_module.key_is_valid = key_is_valid
+
 
 exports = {
     'crypto': crypto_module
