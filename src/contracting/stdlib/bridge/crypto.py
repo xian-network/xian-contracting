c83643ff350f9a55c7142107dc5a61daab982e57
crosschainer <68580992+crosschainer@users.noreply.github.com>
2024-06-04 08:33:02 +0200

    Valid key function (#50)
    * added strptime to stdlib
    
    * Testnet (#47)
    
    * can be reverted
    
    * driver
    
    * driver
    
    * missed
    
    * missed
    
    * missed
    
    * Add loguru dependency
    Replace logger with loguru
    Better formating
    Remove commented our code
    Move constant to constants.py
    
    * update .gitignore & add dependabot config
    
    * added nacl & a new module available to contracts, crypto
    
    ---------
    
    Co-authored-by: endogen <081081@gmx.net>
    Co-authored-by: Endogen <Endogen@users.noreply.github.com>
    Co-authored-by: duelingbenjos <getrektwithme1@gmail.com>
    Co-authored-by: duelingbenjos <75325191+duelingbenjos@users.noreply.github.com>
    
    * Revert "can be reverted" (#28)
    
    This reverts commit 390c83b71bd86e82293ad00ce8c5b9a24461d5db.
    
    * this was sus
    
    ---------
    
    Co-authored-by: endogen <081081@gmx.net>
    Co-authored-by: Endogen <Endogen@users.noreply.github.com>
    Co-authored-by: duelingbenjos <getrektwithme1@gmail.com>
    Co-authored-by: duelingbenjos <75325191+duelingbenjos@users.noreply.github.com>
    
    * Added function to verify if a key is valid
    
    ---------
    
    Co-authored-by: duelingbenjos <getrektwithme1@gmail.com>
    Co-authored-by: duelingbenjos <75325191+duelingbenjos@users.noreply.github.com>
    Co-authored-by: endogen <081081@gmx.net>
    Co-authored-by: Endogen <Endogen@users.noreply.github.com>

--- src/contracting/stdlib/bridge/crypto.py
+++ src/contracting/stdlib/bridge/crypto.py
@@ -15,8 +15,21 @@ def verify(vk: str, msg: str, signature: str):
     return True
 
 
+def key_is_valid(key: str):
+    """ Check if the given address is valid.
+     Can be used with public and private keys """
+    if not len(key) == 64:
+        return False
+    try:
+        int(key, 16)
+    except:
+        return False
+    return True
+
+
 crypto_module = ModuleType('crypto')
 crypto_module.verify = verify
+crypto_module.key_is_valid = key_is_valid
 
 exports = {
     'crypto': crypto_module
