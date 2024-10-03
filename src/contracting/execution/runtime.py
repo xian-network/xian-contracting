0b0c44e8c2552e69e6f737ca875863412b9a5288
crosschainer <eodkwokm@gmail.com>
2024-10-03 14:38:31 +0000

    change

--- src/contracting/execution/runtime.py
+++ src/contracting/execution/runtime.py
@@ -4,7 +4,6 @@ from contracting.execution.tracer import Tracer
 import contracting
 import sys
 import os
-import importlib
 
 
 class Context:
@@ -112,7 +111,6 @@ class Runtime:
             if sys.modules.get(mod) is not None:
                 del sys.modules[mod]
 
-        importlib.invalidate_caches()
         cls.loaded_modules = []
         cls.env = {}
 
